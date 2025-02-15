f0a1825ccf0558363c0fc5bd7275409fc12ec018
unknown <jyoti.shah9753@gmail.com>
2025-02-15 10:56:55 +0530

    added calc app

---
+++ springboot/src/main/java/com/jshah/springboot/controllers/Calc.java
@@ -0,0 +1,34 @@
+package com.jshah.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calc {
+
+    @GetMapping("/add/{n1}/{n2}")
+    public int add(@PathVariable int n1, @PathVariable int n2){
+        return (n1 + n2);
+    }
+    @GetMapping("/subract/{n1}/{n2}")
+    public int subract(@PathVariable int n1, @PathVariable int n2){
+        return (n1 - n2);
+    }
+
+    @GetMapping("/multiply/{n1}/{n2}")
+    public int multiply(@PathVariable int n1, @PathVariable int n2){
+        return (n1 * n2);
+    }
+
+    @GetMapping("/divide/{n1}/{n2}")
+    public int divide(@PathVariable int n1, @PathVariable int n2){
+        return (n1 / n2);
+    }
+
+    @GetMapping("/remainder/{n1}/{n2}")
+    public int remainder(@PathVariable int n1, @PathVariable int n2){
+        return (n1 % n2);
+    }
+
+}
